58977d1cb44e059bf6dc018215172582e138d5ef
Alan Li <li.alan011@gmail.com>
2020-05-12 10:40:43 -0700

    install material ui and updated login component with material ui

--- src/Login.js
+++ src/Login.js
@@ -3,6 +3,18 @@ import { Redirect } from 'react-router-dom';
 import { baseUrl } from './config';
 import CookContext from './CookContext';
 
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+// import Link from '@material-ui/core/Link';
+// import Grid from '@material-ui/core/Grid';
+// import Box from '@material-ui/core/Box';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+
 const Login = () => {
 	const { login, authToken } = useContext(CookContext);
 	const [ loggedIn, setLoggedIn ] = useState(false);
@@ -38,6 +50,28 @@ const Login = () => {
 		setPassword(e.target.value);
 	};
 
+	const useStyles = makeStyles((theme) => ({
+		paper: {
+			marginTop: theme.spacing(8),
+			display: 'flex',
+			flexDirection: 'column',
+			alignItems: 'center'
+		},
+		avatar: {
+			margin: theme.spacing(1),
+			backgroundColor: theme.palette.secondary.main
+		},
+		form: {
+			width: '100%', // Fix IE 11 issue.
+			marginTop: theme.spacing(1)
+		},
+		submit: {
+			margin: theme.spacing(3, 0, 2)
+		}
+	}));
+
+	const classes = useStyles();
+
 	if (loggedIn) {
 		return <Redirect to="/" />;
 	}
@@ -47,32 +81,74 @@ const Login = () => {
 	}
 
 	return (
-		<main>
-			<div className="login-panel center middle">
-				<div>
-					<h1>Login to Start Planning</h1>
-					<form onSubmit={handleSubmit}>
-						<input
-							className="login"
-							type="email"
-							placeholder="Email"
-							value={email}
-							onChange={updateEmail}
-						/>
-						<input
-							className="login"
-							type="password"
-							placeholder="Password"
-							value={password}
-							onChange={updatePassword}
-						/>
-						<button className="btn btn-primary" type="submit">
-							Login
-						</button>
-					</form>
-				</div>
+		// <main>
+		// 	<div className="login-panel center middle">
+		// 		<div>
+		// 			<h1>Login to Start Planning</h1>
+		// 			<form onSubmit={handleSubmit}>
+		// 				<input
+		// 					className="login"
+		// 					type="email"
+		// 					placeholder="Email"
+		// 					value={email}
+		// 					onChange={updateEmail}
+		// 				/>
+		// 				<input
+		// 					className="login"
+		// 					type="password"
+		// 					placeholder="Password"
+		// 					value={password}
+		// 					onChange={updatePassword}
+		// 				/>
+		// 				<Button variant="contained" color="primary" type="submit">
+		// 					Login
+		// 				</Button>
+		// 			</form>
+		// 		</div>
+		// 	</div>
+		// </main>
+		<Container component="main" maxWidth="xs">
+			<CssBaseline />
+			<div className={classes.paper}>
+				<Avatar className={classes.avatar}>
+					<LockOutlinedIcon />
+				</Avatar>
+				<Typography component="h1" variant="h5">
+					Sign in
+				</Typography>
+				<form className={classes.form} noValidate onSubmit={handleSubmit}>
+					<TextField
+						variant="outlined"
+						margin="normal"
+						required
+						fullWidth
+						id="email"
+						label="Email Address"
+						name="email"
+						autoComplete="email"
+						autoFocus
+						onChange={updateEmail}
+						value={email}
+					/>
+					<TextField
+						variant="outlined"
+						margin="normal"
+						required
+						fullWidth
+						name="password"
+						label="Password"
+						type="password"
+						id="password"
+						autoComplete="current-password"
+						value={password}
+						onChange={updatePassword}
+					/>
+					<Button type="submit" fullWidth variant="contained" color="primary" className={classes.submit}>
+						Sign In
+					</Button>
+				</form>
 			</div>
-		</main>
+		</Container>
 	);
 };
 
