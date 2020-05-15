5f3ac4a2acf822a4ac13b4ce60d74c3593082664
Alan Li <li.alan011@gmail.com>
2020-05-15 08:45:23 -0700

    change config to use production

--- src/config.js
+++ src/config.js
@@ -1,1 +1,2 @@
-export const baseUrl = process.env.REACT_APP_BASEURL;
+export const baseUrl =
+	process.env.NODE_ENV === 'production' ? process.env.REACT_APP_PRODUCTION_URL : process.env.REACT_APP_BASEURL;
