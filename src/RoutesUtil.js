f1a3a399c34f967fd99cff62ddb9ffa119336f30
Alan Li <li.alan011@gmail.com>
2020-08-09 22:17:50 -0700

    add comment for the private route component

--- src/RoutesUtil.js
+++ src/RoutesUtil.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
 
+// function to create a route validator. if a route is private(based on App.js) then a user would need be redirected to the login page
 const PrivateRoute = ({ path, component: Component, authToken, componentProps }) => (
 	<Route
 		path={path}
